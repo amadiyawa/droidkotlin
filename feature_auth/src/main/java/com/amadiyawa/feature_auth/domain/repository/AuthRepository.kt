eb7a74aa269392a1b32317ec47377a014c09be74
amadiyawa <amadiyawa@yahoo.com>
2025-05-05 20:30:46 +0200

    feat: update AuthRepository interface to include social sign-in and improve documentation

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/domain/repository/AuthRepository.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/domain/repository/AuthRepository.kt
@@ -1,12 +1,31 @@
 package com.amadiyawa.feature_auth.domain.repository
 
-import com.amadiyawa.feature_auth.domain.model.SignIn
-import com.amadiyawa.feature_auth.data.datasource.api.model.SignInRequest
-import com.amadiyawa.feature_auth.data.datasource.api.model.SignUpRequest
-import com.amadiyawa.feature_auth.domain.model.SignUp
+import com.amadiyawa.feature_auth.data.dto.request.SignInRequest
+import com.amadiyawa.feature_auth.domain.model.AuthResult
+import com.amadiyawa.feature_auth.domain.util.SocialProvider
 import com.amadiyawa.feature_base.domain.result.OperationResult
 
+/**
+ * Interface for handling authentication operations.
+ *
+ * This interface defines methods for signing in users through traditional
+ * credentials or social providers. It encapsulates the authentication logic
+ * and returns the result of the operation.
+ */
 internal interface AuthRepository {
-    suspend fun signIn(signInRequest: SignInRequest): OperationResult<SignIn>
-    suspend fun signUp(signUpRequest: SignUpRequest): OperationResult<SignUp>
+    /**
+     * Signs in a user using the provided credentials.
+     *
+     * @param signInRequest The request object containing the user's credentials.
+     * @return An `OperationResult` containing the authentication result.
+     */
+    suspend fun signIn(signInRequest: SignInRequest): OperationResult<AuthResult>
+
+    /**
+     * Signs in a user using a social provider.
+     *
+     * @param provider The social provider to be used for authentication.
+     * @return An `OperationResult` containing the authentication result.
+     */
+    suspend fun socialSignIn(provider: SocialProvider): OperationResult<AuthResult>
 }
