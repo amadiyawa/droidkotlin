f276d38e49e78c18d8eb6a48e79a95b8cb391f2e
amadiyawa <amadiyawa@yahoo.com>
2025-05-06 00:32:25 +0200

    feat: update data module to use SimulatedAuthRepository for testing purposes

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/data/DataModule.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/data/DataModule.kt
@@ -1,12 +1,12 @@
 package com.amadiyawa.feature_auth.data
 
-import com.amadiyawa.feature_auth.data.remote.AuthApiService
-import com.amadiyawa.feature_auth.data.repository.AuthRepositoryImpl
+import com.amadiyawa.feature_auth.data.source.remote.AuthApiService
+import com.amadiyawa.feature_auth.data.repository.SimulatedAuthRepository
 import com.amadiyawa.feature_auth.domain.repository.AuthRepository
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 internal val dataModule = module {
-    single<AuthRepository> { AuthRepositoryImpl(api = get()) }
+    single<AuthRepository> { SimulatedAuthRepository() }
     single { get<Retrofit>().create(AuthApiService::class.java)}
 }
