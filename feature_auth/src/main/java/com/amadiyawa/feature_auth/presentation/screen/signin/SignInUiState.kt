55a3ec86a0d5e02578e4b2256ca2503ef3cd1556
amadiyawa <amadiyawa@yahoo.com>
2025-05-05 20:17:43 +0200

    feat: update SignInUiState to improve state management and add documentation for UI state representation

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/signin/SignInUiState.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/signin/SignInUiState.kt
@@ -1,9 +1,15 @@
 package com.amadiyawa.feature_auth.presentation.screen.signin
 
-import com.amadiyawa.feature_auth.domain.enum.SocialProvider
+import com.amadiyawa.feature_auth.domain.util.SocialProvider
 import com.amadiyawa.feature_auth.domain.model.SignInForm
 import com.amadiyawa.feature_base.presentation.screen.viewmodel.BaseState
 
+/**
+ * Represents the UI state for the Sign-In screen.
+ *
+ * This sealed class defines the various states that the Sign-In screen can be in.
+ * It extends the `BaseState` interface to integrate with the application's state management.
+ */
 sealed class SignInUiState : BaseState {
     data class Idle(val form: SignInForm = SignInForm()) : SignInUiState()
 
