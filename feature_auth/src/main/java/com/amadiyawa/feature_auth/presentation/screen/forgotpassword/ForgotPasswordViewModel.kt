6a99d2ae2d65a09849d900642108c697165f1200
amadiyawa <amadiyawa@yahoo.com>
2025-05-06 17:21:22 +0200

    feat: refactor ForgotPasswordViewModel to integrate ForgotPasswordUseCase and improve form handling

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/forgotpassword/ForgotPasswordViewModel.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/forgotpassword/ForgotPasswordViewModel.kt
@@ -1,30 +1,22 @@
 package com.amadiyawa.feature_auth.presentation.screen.forgotpassword
 
 import androidx.lifecycle.viewModelScope
+import com.amadiyawa.feature_auth.data.dto.request.ForgotPasswordRequest
 import com.amadiyawa.feature_auth.domain.model.ForgotPasswordForm
-import com.amadiyawa.feature_auth.domain.model.SignUp
+import com.amadiyawa.feature_auth.domain.model.VerificationResult
 import com.amadiyawa.feature_auth.domain.model.updateAndValidateField
+import com.amadiyawa.feature_auth.domain.usecase.ForgotPasswordUseCase
 import com.amadiyawa.feature_auth.domain.util.OtpPurpose
 import com.amadiyawa.feature_auth.domain.util.validation.ForgotPasswordFormValidator
+import com.amadiyawa.feature_base.domain.result.OperationResult
 import com.amadiyawa.feature_base.presentation.screen.viewmodel.BaseViewModel
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 internal class ForgotPasswordViewModel(
-    private val validator: ForgotPasswordFormValidator
+    private val validator: ForgotPasswordFormValidator,
+    private val forgotPasswordUseCase: ForgotPasswordUseCase
 ) : BaseViewModel<ForgotPasswordUiState, ForgotPasswordAction>(ForgotPasswordUiState.Idle()) {
-
-    private val _uiEvent = MutableSharedFlow<ForgotPasswordUiEvent>()
-    val uiEvent = _uiEvent.asSharedFlow()
-
-    private val _isSubmitting = MutableStateFlow(false)
-    val isSubmitting: StateFlow<Boolean> = _isSubmitting.asStateFlow()
 
     val form: ForgotPasswordForm
         get() = when (val s = state) {
@@ -39,15 +31,21 @@ internal class ForgotPasswordViewModel(
         logAction(action)
 
         when (action) {
-            is ForgotPasswordAction.UpdateField -> {
-                val updatedForm = form.updateAndValidateField(action.key, action.value, validator)
-                setState { ForgotPasswordUiState.Idle(updatedForm) }
-            }
+            is ForgotPasswordAction.UpdateField -> handleUpdateField(action)
             is ForgotPasswordAction.Submit -> handleSubmit()
             is ForgotPasswordAction.ShowError -> {
                 setState { ForgotPasswordUiState.Error(form, action.message) }
             }
         }
+    }
+
+    private fun handleUpdateField(action: ForgotPasswordAction.UpdateField) {
+        val updatedForm = form.updateAndValidateField(
+            key = action.key,
+            fieldValue = action.value,
+            validator = validator
+        )
+        setState { ForgotPasswordUiState.Idle(updatedForm) }
     }
 
     fun handleSubmit() {
@@ -57,17 +55,28 @@ internal class ForgotPasswordViewModel(
             return
         }
 
-        _isSubmitting.value = true
         setState { ForgotPasswordUiState.Loading(form = form) }
 
         forgotPasswordJob?.cancel()
         forgotPasswordJob = viewModelScope.launch {
-            delay(2000) // simulate API call
-            val signUp = SignUp.random()
-            signUp.otpPurpose = OtpPurpose.FORGOT_PASSWORD
-            signUp.recipient = form.identifier.value
-            _uiEvent.emit(ForgotPasswordUiEvent.NavigateToOtp(signUp))
-            _isSubmitting.value = false
+            val requestData = ForgotPasswordRequest(recipient = form.identifier.value)
+            when (val result = forgotPasswordUseCase(requestData)) {
+                is OperationResult.Success -> handleAuthSuccess(result.data)
+                is OperationResult.Error -> handleAuthError(result.message!!)
+                is OperationResult.Failure -> handleAuthError(result.message!!)
+            }
+
+            setState { ForgotPasswordUiState.Idle(form) }
         }
+    }
+
+    private fun handleAuthSuccess(data: VerificationResult) {
+        data.purpose = OtpPurpose.FORGOT_PASSWORD
+        emitEvent(ForgotPasswordUiEvent.NavigateToOtp(data))
+    }
+
+    private fun handleAuthError(message: String) {
+        setState { ForgotPasswordUiState.Error(form, message) }
+        emitEvent(ForgotPasswordUiEvent.ShowSnackbar(message))
     }
 }
