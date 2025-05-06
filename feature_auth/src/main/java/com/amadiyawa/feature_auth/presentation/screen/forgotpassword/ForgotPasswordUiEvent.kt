2a5a8e396e247d8431f0160398e37a80d32e35c7
amadiyawa <amadiyawa@yahoo.com>
2025-05-06 17:22:48 +0200

    feat: update ForgotPasswordUiEvent to use VerificationResult for navigation

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/forgotpassword/ForgotPasswordUiEvent.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/forgotpassword/ForgotPasswordUiEvent.kt
@@ -1,8 +1,8 @@
 package com.amadiyawa.feature_auth.presentation.screen.forgotpassword
 
-import com.amadiyawa.feature_auth.domain.model.SignUp
+import com.amadiyawa.feature_auth.domain.model.VerificationResult
 
 internal sealed interface ForgotPasswordUiEvent {
-    data class NavigateToOtp(val signUp: SignUp) : ForgotPasswordUiEvent
+    data class NavigateToOtp(val data: VerificationResult) : ForgotPasswordUiEvent
     data class ShowSnackbar(val message: String) : ForgotPasswordUiEvent
 }
