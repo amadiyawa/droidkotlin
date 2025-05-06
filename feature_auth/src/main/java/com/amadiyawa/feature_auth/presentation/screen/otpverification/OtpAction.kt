238f13bb6f4199de150787e9f064b5df6860bb51
amadiyawa <amadiyawa@yahoo.com>
2025-05-06 17:27:22 +0200

    feat: update OtpAction to use VerificationResult for initialization

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/otpverification/OtpAction.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/otpverification/OtpAction.kt
@@ -1,11 +1,11 @@
 package com.amadiyawa.feature_auth.presentation.screen.otpverification
 
-import com.amadiyawa.feature_auth.domain.model.SignUp
+import com.amadiyawa.feature_auth.domain.model.VerificationResult
 import com.amadiyawa.feature_base.presentation.screen.viewmodel.BaseAction
 
 internal sealed interface OtpAction : BaseAction {
     data class UpdateDigit(val index: Int, val value: String) : OtpAction
     data object Submit : OtpAction
     data class ShowError(val message: String) : OtpAction
-    data class Initialize(val signUp: SignUp) : OtpAction
+    data class Initialize(val data: VerificationResult) : OtpAction
 }
