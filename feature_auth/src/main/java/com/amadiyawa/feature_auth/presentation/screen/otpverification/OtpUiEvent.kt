c496b1a81e560f74a104b904cc59b12d30b8eb58
amadiyawa <amadiyawa@yahoo.com>
2025-05-06 17:23:31 +0200

    feat: update OtpUiEvent to use VerificationResult for navigation

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/otpverification/OtpUiEvent.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/otpverification/OtpUiEvent.kt
@@ -1,8 +1,8 @@
 package com.amadiyawa.feature_auth.presentation.screen.otpverification
 
-import com.amadiyawa.feature_auth.domain.model.SignUp
+import com.amadiyawa.feature_auth.domain.model.VerificationResult
 
 internal sealed interface OtpUiEvent {
 
-    data class NavigateToNextScreen(val signUp: SignUp) : OtpUiEvent
+    data class NavigateToNextScreen(val data: VerificationResult) : OtpUiEvent
 }
