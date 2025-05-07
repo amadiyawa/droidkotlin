1181cd85cfbd263e9f14ce98eb0b589acac87281
amadiyawa <amadiyawa@yahoo.com>
2025-05-08 01:03:50 +0200

    feat: add OTP verification and resend functionality to AuthRepository

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/domain/repository/AuthRepository.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/domain/repository/AuthRepository.kt
@@ -1,8 +1,11 @@
 package com.amadiyawa.feature_auth.domain.repository
 
 import com.amadiyawa.feature_auth.data.dto.request.ForgotPasswordRequest
+import com.amadiyawa.feature_auth.data.dto.request.OtpVerificationRequest
+import com.amadiyawa.feature_auth.data.dto.request.ResendOtpRequest
 import com.amadiyawa.feature_auth.data.dto.request.SignInRequest
 import com.amadiyawa.feature_auth.domain.model.AuthResult
+import com.amadiyawa.feature_auth.domain.model.OtpVerificationResult
 import com.amadiyawa.feature_auth.domain.model.VerificationResult
 import com.amadiyawa.feature_auth.domain.util.SocialProvider
 import com.amadiyawa.feature_base.domain.result.OperationResult
@@ -43,4 +46,8 @@ internal interface AuthRepository {
      * @return An `OperationResult` containing the verification response.
      */
     suspend fun forgotPassword(data: ForgotPasswordRequest): OperationResult<VerificationResult>
+
+    suspend fun verifyOtp(data: OtpVerificationRequest): OperationResult<OtpVerificationResult>
+
+    suspend fun resendOtp(request: ResendOtpRequest): OperationResult<VerificationResult>
 }
