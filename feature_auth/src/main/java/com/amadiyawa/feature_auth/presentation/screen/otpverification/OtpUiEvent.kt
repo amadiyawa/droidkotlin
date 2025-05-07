045d7fe0d852e84fb8561b1304e3083371cf279d
amadiyawa <amadiyawa@yahoo.com>
2025-05-08 00:47:22 +0200

    feat: update OtpUiEvent to include new navigation and error handling events

--- feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/otpverification/OtpUiEvent.kt
+++ feature_auth/src/main/java/com/amadiyawa/feature_auth/presentation/screen/otpverification/OtpUiEvent.kt
@@ -1,8 +1,11 @@
 package com.amadiyawa.feature_auth.presentation.screen.otpverification
 
-import com.amadiyawa.feature_auth.domain.model.VerificationResult
+import com.amadiyawa.feature_auth.domain.model.AuthResult
 
 internal sealed interface OtpUiEvent {
-
-    data class NavigateToNextScreen(val data: VerificationResult) : OtpUiEvent
+    data class NavigateToHome(val authResult: AuthResult) : OtpUiEvent
+    data class NavigateToResetPassword(val resetToken: String) : OtpUiEvent
+    data class ShowError(val message: String, val isFatal: Boolean = false) : OtpUiEvent
+    data object ClearError : OtpUiEvent
+    data class UpdateResendAvailability(val canResend: Boolean, val countdown: Int?) : OtpUiEvent
 }
